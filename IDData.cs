7df1d827cb59433b5d0f748db090d70e5e693a6f
solutena <solutena@naver.com>
2022-12-01 00:24:14 +0900

    Add files via upload

---
+++ IDData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class IDData
+{
+	[SerializeField] string id;
+
+	public string ID { get => id; private set => id = value; }
+
+	public IDData() { ID = Guid.NewGuid().ToString(); }
+	public static bool operator ==(IDData a, IDData b) => Equals(a, b);
+	public static bool operator !=(IDData a, IDData b) => !Equals(a, b);
+	public static bool Equals(IDData a, IDData b)
+	{
+		if (IsNull(a))
+			return IsNull(b);
+		else
+		{
+			if (IsNull(b))
+				return false;
+			return a.Equals(b);
+		}
+	}
+	public static bool IsNull(IDData data) => data is null || string.IsNullOrEmpty(data.ID);
+	public override bool Equals(object obj) => ID == (obj as IDData).ID;
+	public override int GetHashCode() => ID.GetHashCode();
+}
