becae2b3d210b459844e78e7903366d4a01ee16f
solutena <solutena@naver.com>
2024-01-30 01:23:22 +0900

    Add files via upload

--- IDData.cs
+++ IDData.cs
@@ -4,22 +4,20 @@ using UnityEngine;
 [Serializable]
 public class IDData
 {
-	[SerializeField] string id;
+	public string id;
 
-	public string ID { get => id; set => id = value; }
-
-	public IDData() { ID = Guid.NewGuid().ToString(); }
+	public IDData() { id = Guid.NewGuid().ToString(); }
 	public override bool Equals(object obj)
 	{
 		if (obj is IDData compare)
-			return ID.Equals(compare.ID);
+			return id.Equals(compare.id);
 		return false;
 	}
-	public override int GetHashCode() => string.IsNullOrEmpty(ID) ? 0 : ID.GetHashCode();
+	public override int GetHashCode() => string.IsNullOrEmpty(id) ? 0 : id.GetHashCode();
 	public static bool operator ==(IDData a, IDData b)
 	{
-		if (a is null || string.IsNullOrEmpty(a.ID))
-			return b is null || string.IsNullOrEmpty(b.ID);
+		if (a is null || string.IsNullOrEmpty(a.id))
+			return b is null || string.IsNullOrEmpty(b.id);
 		return a.Equals(b);
 	}
 	public static bool operator !=(IDData a, IDData b) => !(a == b);
